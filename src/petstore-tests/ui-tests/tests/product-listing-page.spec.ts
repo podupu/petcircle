a393082356bb726576b6af8967954739c02fecd4
Avinash Eediga <avinasheediga@avinashs-MacBook-Pro-2.local>
2024-09-14 18:35:58 +1000

    final commit

--- src/petstore-tests/ui-tests/tests/product-listing-page.spec.ts
+++ src/petstore-tests/ui-tests/tests/product-listing-page.spec.ts
@@ -1,17 +1,28 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../pages/loginPage";
-import { InventoryPage } from "../pages/inventoryPage";
 
-test.describe("Product Listing Page ", () => {
-  test("- Add Item to Cart", async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    const inventoryPage = new InventoryPage(page);
-    await loginPage.navigate();
-    await loginPage.login("standard_user", "secret_sauce");
-    await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
-    await expect(page.locator(".inventory_item")).toHaveCount(6);
-    await inventoryPage.addFirstTwoItemsToCart();
-    const cartCount = await page.locator(".shopping_cart_badge").textContent();
-    expect(cartCount).toBe("2");
+test('Verify product listing page - Names, Number, and Text', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.navigate();
+  await loginPage.login("standard_user", "secret_sauce");
+  const productNames = await page.locator('.inventory_item_name').allTextContents();
+  const productCount = productNames.length;
+  console.log('Product Count:', productCount);
+  expect(productCount).toBe(6);
+  const expectedNames = [
+    "Sauce Labs Backpack",
+    "Sauce Labs Bike Light",
+    "Sauce Labs Bolt T-Shirt",
+    "Sauce Labs Fleece Jacket",
+    "Sauce Labs Onesie",
+    "Test.allTheThings() T-Shirt (Red)"
+  ];
+  expectedNames.forEach((name, index) => {
+    expect(productNames[index]).toBe(name);
   });
+  const productPrices = await page.locator('.inventory_item_price').allTextContents();
+  productPrices.forEach(price => {
+    expect(price).toMatch(/^\$\d+\.\d{2}$/);
+  });
+  console.log('Product Names:', productNames);
 });
